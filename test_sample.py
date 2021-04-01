ebed01bcf779d057c74cccc7176a57f6ea7f578c
MERIDIANLINK\congwan11 <cong.wang@meridianlink.com>
2021-04-01 10:59:39 -0400

    simulate pytest project before writing jenkinsfile

---
+++ test_sample.py
@@ -0,0 +1,10 @@
+# content of test_sample.py
+def func(x):
+    return x + 1
+
+
+def test_answer_sample_of_failure():
+    assert func(3) == 5
+
+def test_answer_sample_of_success():
+    assert func(3) == 4
